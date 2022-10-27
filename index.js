f50babc942e1c86f084511c742d982be3ec39aa5
Henry Saver <hesa1262@colorado.edu>
2022-10-27 10:53:17 -0600

    Basic Server routing

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+/*START ROUTING*/
+// Default Channel
 app.get("/", (req, res) => {
-//   res.send("Hi there! Welcome to the home of Project SafeGuard");
   res.sendFile('views/homePage.html', {root: __dirname })
-//   res.redirect('views/homePage,html', {root: __dirname});
 });
+
+// Home Page
 app.get('/home', function(req, res) {
     res.sendFile('views/homePage.html', {root: __dirname })
 });
 
+//Communication with ESP
+app.get('/testReq', function(req, res){
+    res.send("Successfully pinged Project SafeGuard's Server! \n Current Status: ONLINE")
+})
+/*END ROUTING*/
 app.listen(port, () => {
   console.log(`Capstone Server currently listening on ${port}.`);
 });
