9e087c37731e943a4602d634982348f2e1c93a6b
Henry Saver <hesa1262@colorado.edu>
2022-11-04 14:01:41 -0600

    backup

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+const { timeStamp } = require("console");
 const express = require("express");
 const app = express();
-const port = 3000;
+const http = require('http').Server(express)
+const port = 80;
+var ip = require("ip");
+// console.dir ( ip.address() );
+const ipaddress = ip.address()
+console.log(ip.address())
 
 /*START ROUTING*/
 // Default Channel
 app.get("/", (req, res) => {
+  console.log("SERVER PINGED at: " + timeStamp)
   res.sendFile('views/homePage.html', {root: __dirname })
 });
 
@@ -15,9 +22,19 @@ app.get('/home', function(req, res) {
 
 //Communication with ESP
 app.get('/testReq', function(req, res){
-    res.send("Successfully pinged Project SafeGuard's Server! \n Current Status: ONLINE")
+    res.send("Successfully pinged Project SafeGuard's Server!\n Current Status: ONLINE")
 })
 /*END ROUTING*/
+
+/*Sylesheet Routing */
+app.use(express.static(__dirname + '/styles'));
+
+app.use('/img' ,express.static(__dirname + '/views/img'));
+
 app.listen(port, () => {
   console.log(`Capstone Server currently listening on ${port}.`);
 });
+
+// http.listen(port,function () {
+//   console.log(`Capstone Server currently listening on ${port}.`);
+// });
