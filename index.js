492c22a640e6ee7b1a8fc4bd98212ca181b554e2
Henry Saver <hesa1262@colorado.edu>
2022-11-15 11:09:41 -0700

    New Route working with Cellular Module

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const http = require('http').Server(express)
 const port = 80;
 var ip = require("ip");
 // console.dir ( ip.address() );
+
+app.use(express.json());
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+
 const ipaddress = ip.address()
 console.log(ip.address())
 
 /*START ROUTING*/
 // Default Channel
 app.get("/", (req, res) => {
-  console.log("SERVER PINGED at: " + timeStamp)
+  console.log("SERVER PINGED at: " + new Date().toString)
   res.sendFile('views/homePage.html', {root: __dirname })
 });
 
@@ -24,6 +29,20 @@ app.get('/home', function(req, res) {
 app.get('/testReq', function(req, res){
     res.send("Successfully pinged Project SafeGuard's Server!\n Current Status: ONLINE")
 })
+
+
+//Cellular Communication - Body Contains Various Data from device. See documentation for what
+app.post("/", jsonParser , (req, res) => {
+  console.log("SERVER PINGED")
+  console.log(req.body)
+  res.send("Hello World! - SD Server")
+
+    // console.log(JSON.parse(req.body))
+  // console.log(JSON.stringify(JSON.parse(req.body)))
+  // console.log("===================================")
+  // console.log(req.db)
+  // console.log(req)
+});
 /*END ROUTING*/
 
 /*Sylesheet Routing */
